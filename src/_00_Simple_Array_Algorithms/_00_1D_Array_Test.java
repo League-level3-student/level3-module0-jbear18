3e81d1cb9e182f30eed49fa8d252b70ac6754e4f
dave <dave@192.168.0.14>
2018-06-05 09:28:08 -0700

    initial

---
+++ src/_00_Simple_Array_Algorithms/_00_1D_Array_Test.java
@@ -0,0 +1,55 @@
+package _00_Simple_Array_Algorithms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class _00_1D_Array_Test {
+
+	@Test
+	void testSum() {
+		int[] arr = {0, 1, 2, 3, 4};
+		assertEquals(10, _00_1D_Array_Methods.sumIntArray(arr));
+		
+		int[] arr2 = {-4, 1, 2, 1, -3};
+		assertEquals(-3, _00_1D_Array_Methods.sumIntArray(arr2));
+	}
+	
+	@Test
+	void testAverage() {
+		int[] arr = {0, 1, 2, 3, 4};
+		assertEquals(2.0, _00_1D_Array_Methods.averageIntArray(arr), 0.001);
+
+		int[] arr2 = {5, 3, 23, -6, 24};
+		assertEquals(9.8, _00_1D_Array_Methods.averageIntArray(arr2), 0.001);
+		
+		int[] arr3 = {20, 1, -12, 93, -424};
+		assertEquals(-64.4, _00_1D_Array_Methods.averageIntArray(arr3), 0.001);
+		
+	}
+	
+	@Test
+	void testContains() {
+		int[] arr = {0, 0, 0, 0, 0, 0, 0};
+		assertTrue(_00_1D_Array_Methods.containsIntValue(arr, 0));
+
+		int[] arr2 = {0, 0, 0, 0, 0, 0, 0};
+		assertFalse(_00_1D_Array_Methods.containsIntValue(arr2, 1));
+		
+		int[] arr3 = {0, 1, 2, 3, 4};
+		assertTrue(_00_1D_Array_Methods.containsIntValue(arr3, 4));
+	}
+	
+	@Test 
+	void testGetIndex(){
+		int[] arr = {0, 0, 0};
+		assertEquals(0, _00_1D_Array_Methods.getIndex(arr, 0));
+		
+		int[] arr2 = {1, 2, 3};
+		assertEquals(2, _00_1D_Array_Methods.getIndex(arr2, 3));
+		
+		int[] arr3 = {0, 0, 0};
+		assertEquals(-1, _00_1D_Array_Methods.getIndex(arr3, 1));
+	}
+
+}
---
+++ src/_01_Firework_Display/FireworkDisplay.java
@@ -0,0 +1,84 @@
+package _01_Firework_Display;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class FireworkDisplay extends JPanel implements ActionListener {
+	public static int WIDTH = 720;
+	public static int HEIGHT = 480;
+	
+	private JFrame window;
+	private JPanel buttonPanel;
+	private JButton squirtButton;
+	private Timer timer;
+	
+	Firework firework = new Firework();
+	
+	public static void main(String[] args) {
+		new FireworkDisplay().start();
+	}
+	
+	public void start() {
+		window = new JFrame();
+		window.setLayout(new BorderLayout());
+		window.add(this, BorderLayout.CENTER);
+		
+		WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
+		HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		buttonPanel = new JPanel();
+		squirtButton = new JButton("FIRE");
+		squirtButton.addActionListener((e)->{
+			fire();
+		});
+		
+		buttonPanel.add(squirtButton);
+		window.add(buttonPanel, BorderLayout.SOUTH);
+		
+		window.pack();
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		
+		timer = new Timer(1000 / 60, this);
+	}
+	
+	private void fire() {
+		squirtButton.setEnabled(false);
+		firework = new Firework();
+		firework.launch();
+		timer.start();		
+	}
+	
+	@Override
+	public void paintComponent(Graphics g) {
+		g.setColor(new Color(10, 30, 70));
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		firework.drawSparks(g);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		firework.updateSparks();
+		repaint();
+		
+		boolean reset = true;
+		if(!firework.dead) {
+			reset = false;
+		}
+		if(reset) {
+			timer.stop();
+			squirtButton.setEnabled(true);
+		}
+	}
+}
