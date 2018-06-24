486fd60227c78fb2bbea0498ce44fc7cec2b1617
DaveDLeague <dave.dunn@jointheleague.org>
2018-06-24 12:51:45 -0700

    Update _00_1D_Array_Methods.java

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -3,14 +3,14 @@ package _01_Simple_Array_Algorithms;
 public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
-	static int sumIntArray(int[] values) {
+	public static int sumIntArray(int[] values) {
 
 		return 0;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
-	static double averageIntArray(int[] values) {
+	public static double averageIntArray(int[] values) {
 				
 		return 0;
 	}
@@ -19,7 +19,7 @@ public class _00_1D_Array_Methods {
 	//3. Complete the method so that it returns true if the integer
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
-	static boolean containsIntValue(int[] array, int value) {
+	public static boolean containsIntValue(int[] array, int value) {
 	
 		return false;
 	}
@@ -27,7 +27,7 @@ public class _00_1D_Array_Methods {
 	//4. Complete the method so that it returns the index of the,
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
-	static int getIndex(int[] arr, int value) {
+	public static int getIndex(int[] arr, int value) {
 		
 		return 0;
 	}
