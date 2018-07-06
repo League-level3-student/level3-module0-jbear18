93ce5f35473c250d88dbc9cfdd0b47d637449ed5
DaveDLeague <dave.dunn@jointheleague.org>
2018-07-05 18:10:34 -0700

    Update _01_RobotRace.java

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -8,7 +8,7 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	//1. make a main method
-	public static void main(String[] args) {
+	
 		//2. create an array of 5 robots.
 
 		//3. use a for loop to initialize the robots.
@@ -26,5 +26,5 @@ public class _01_RobotRace {
     	
 	    //9. make the robots race around a circular track.
 
-	}
+	
 }
