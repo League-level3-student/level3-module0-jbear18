0655b4fdc351e85212681aedc2c953ed84dfe533
Michiyo Wellington-Oguri <mwoguri@gmail.com>
2019-01-06 15:40:32 -0800

    vary array lengths to prevent hard-coding

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Test.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Test.java
@@ -8,8 +8,8 @@ public class _00_1D_Array_Test {
 
 	@Test
 	public void testSum() {
-		int[] arr = {0, 1, 2, 3, 4};
-		assertEquals(10, _00_1D_Array_Methods.sumIntArray(arr));
+		int[] arr = {0, 1, 2, 3};
+		assertEquals(6, _00_1D_Array_Methods.sumIntArray(arr));
 		
 		int[] arr2 = {-4, 1, 2, 1, -3};
 		assertEquals(-3, _00_1D_Array_Methods.sumIntArray(arr2));
@@ -23,17 +23,17 @@ public class _00_1D_Array_Test {
 		int[] arr2 = {5, 3, 23, -6, 24};
 		assertEquals(9.8, _00_1D_Array_Methods.averageIntArray(arr2), 0.001);
 		
-		int[] arr3 = {20, 1, -12, 93, -424};
-		assertEquals(-64.4, _00_1D_Array_Methods.averageIntArray(arr3), 0.001);
+		int[] arr3 = {20, 1, -12, 93};
+		assertEquals(25.5, _00_1D_Array_Methods.averageIntArray(arr3), 0.001);
 		
 	}
 	
 	@Test
 	public void testContains() {
-		int[] arr = {0, 0, 0, 0, 0, 0, 0};
+		int[] arr = {0, 0, 0, 0, 0, 0};
 		assertTrue(_00_1D_Array_Methods.containsIntValue(arr, 0));
 
-		int[] arr2 = {0, 0, 0, 0, 0, 0, 0};
+		int[] arr2 = {0, 0, 0, 0, 0, 0};
 		assertFalse(_00_1D_Array_Methods.containsIntValue(arr2, 1));
 		
 		int[] arr3 = {0, 1, 2, 3, 4};
@@ -45,8 +45,8 @@ public class _00_1D_Array_Test {
 		int[] arr = {0, 0, 0};
 		assertEquals(0, _00_1D_Array_Methods.getIndex(arr, 0));
 		
-		int[] arr2 = {1, 2, 3};
-		assertEquals(2, _00_1D_Array_Methods.getIndex(arr2, 3));
+		int[] arr2 = {1, 2, 3, 4};
+		assertEquals(3, _00_1D_Array_Methods.getIndex(arr2, 4));
 		
 		int[] arr3 = {0, 0, 0};
 		assertEquals(-1, _00_1D_Array_Methods.getIndex(arr3, 1));
