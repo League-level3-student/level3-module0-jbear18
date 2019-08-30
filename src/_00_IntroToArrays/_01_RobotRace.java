de347037bc1328343491dd052f53067481bf9c6b
League Student <league@iMac-7.attlocal.net>
2019-08-30 16:54:09 -0700

    robot race ip

--- src/_00_IntroToArrays/_01_RobotRace.java
+++ src/_00_IntroToArrays/_01_RobotRace.java
@@ -15,9 +15,9 @@ public class _01_RobotRace {
 
 		// 3. use a for loop to initialize the robots.
 		for (int i = 0; i < robotArray.length; i++) {
-			robotArray[i] = new Robot("batman");
-			robotArray[i].setX(i * 200 + 40);
-			robotArray[i].setY(500);
+			robotArray[i] = new Robot();
+			robotArray[i].setX(i * 100 + 5);
+			robotArray[i].setY(300);
 		}
 		// 4. make each robot start at the bottom of the screen, side by side, facing up
 
@@ -27,29 +27,32 @@ public class _01_RobotRace {
 		boolean isRacing = true;
 
 		while (isRacing) {
-			for (int i = 0; i < robotArray.length; i++) {
-				int randy = rand.nextInt(50);
-				robotArray[i].setSpeed(10);
-				robotArray[i].sparkle();
-				robotArray[i].setRandomPenColor();
-				robotArray[i].penDown();
-				robotArray[i].move(randy);
-				if(robotArray[i].getY()<=0) {
-					isRacing=false;
-					JOptionPane.showMessageDialog(null, "Robot" + i+" has won the race!");
-					break;
+			for (int j = 0; j < 36; j++) {
+
+				for (int i = 0; i < robotArray.length; i++) {
+					int randy = rand.nextInt(50);
+					robotArray[i].move(70 );
+					robotArray[i].turn(10);
+					
+					robotArray[i].setSpeed(randy);
+					robotArray[i].sparkle();
+					robotArray[i].setRandomPenColor();
+					robotArray[i].penDown();
+					if (robotArray[i].getY() <= 0) {
+						isRacing = false;
+					}
 				}
 			}
+
+			// 6. use a while loop to repeat step 5 until a robot has reached the top of the
+			// screen.
+
+			// 7. declare that robot the winner and throw it a party!
+
+			// 8. try different races with different amounts of robots.
+
+			// 9. make the robots race around a circular track.
+
 		}
-
-		// 6. use a while loop to repeat step 5 until a robot has reached the top of the
-		// screen.
-
-		// 7. declare that robot the winner and throw it a party!
-
-		// 8. try different races with different amounts of robots.
-
-		// 9. make the robots race around a circular track.
-
 	}
 }
