a754b212defa10919ee3217b85aebbc59cda37fe
League Student <league@iMac-21.attlocal.net>
2019-09-13 16:59:16 -0700

    Array Methods

--- src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
+++ src/_01_Simple_Array_Algorithms/_00_1D_Array_Methods.java
@@ -4,15 +4,19 @@ public class _00_1D_Array_Methods {
 	//1. Complete the method so that it returns the sum of all
 	//   of the integers in the array being passed in
 	public static int sumIntArray(int[] values) {
-
-		return 0;
+		int sum=0;
+for (int i = 0; i < values.length; i++) {
+sum= sum + values [i];
+}
+		return sum;
 	}
 	
 	//2. Complete the method so that it returns the average of all
 	//   of the integers in the array being passed in
 	public static double averageIntArray(int[] values) {
-				
-		return 0;
+		
+		double ans =(double)sumIntArray (values)/(double)values.length	;
+		return ans;
 	}
 	
 	
@@ -20,7 +24,11 @@ public class _00_1D_Array_Methods {
 	//   array contains the value specified by the second parameter.
 	//   It should otherwise return false.
 	public static boolean containsIntValue(int[] array, int value) {
-	
+	for (int i = 0; i < array.length; i++) {
+		if(value== array [i]) {
+			return true;
+		}
+	}
 		return false;
 	}
 	
@@ -28,7 +36,13 @@ public class _00_1D_Array_Methods {
 	//   first instance that the specified value occurs in the array.
 	//   If the array does not contain the specified value, it should return -1.
 	public static int getIndex(int[] arr, int value) {
+		for (int i = 0; i < arr.length; i++) {
+			if(value== arr [i]) {
+				return i;
+			}
+		}
+			return -1;
+		}
 		
-		return 0;
 	}
-}
+
