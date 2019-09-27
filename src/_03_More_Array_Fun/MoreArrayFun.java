5e4d2ba15f960d387794386c7e3e6d8ab969411a
League Student <league@iMac-21.attlocal.net>
2019-09-27 16:58:36 -0700

    MoreArrayFun IP, Firework Display

--- src/_03_More_Array_Fun/MoreArrayFun.java
+++ src/_03_More_Array_Fun/MoreArrayFun.java
@@ -4,16 +4,38 @@ import java.util.Random;
 
 public class MoreArrayFun {
 	//1. Create a main method to test the other methods you write.
-	
+	public static void main(String[] args) {
+String[] stringArray= new String[20];
+stringArray[0]= "blue";
+stringArray[1]= "red";
+stringArray[2]= "yellow";
+arrayReverse(stringArray);
+
+System.out.println("-THIS WAS INTENTIONALLY LEFT BLANK-");
+
+arrayTaker(stringArray);
+	}
 	
 	
 	//2. Write a method that takes an array of Strings and prints all the Strings in the array.
+
+	public static void arrayTaker(String[] stringArray) {
 	
-	
+	for (int i = 0; i < stringArray.length; i++) {
+		System.out.println(stringArray[i]);
+	}	
+
+	}
 	
 	//3. Write a method that takes an array of Strings and prints all the Strings in the array
 	//   in reverse order.
-	
+	public static void arrayReverse(String[] stringArray) {
+		
+		for (int i = stringArray.length-1; i >= 0; i--) {
+			System.out.println(stringArray[i]);
+		}	
+
+		}
 	
 	
 	//4. Write a method that takes an array of Strings and prints every other String in the array.
